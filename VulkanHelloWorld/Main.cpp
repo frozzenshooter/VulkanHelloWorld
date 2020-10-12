e478e332de2b2ee0a453a3b4df82e11b43fa7a16
Steffen Knoblauch <knoblauch.steffen@web.de>
2020-10-12 17:03:16 +0200

    Swapchain initalization

--- VulkanHelloWorld/Main.cpp
+++ VulkanHelloWorld/Main.cpp
@@ -9,7 +9,11 @@
 VkInstance instance;
 VkSurfaceKHR surface;
 VkDevice device;
+VkSwapchainKHR swapchain;
 GLFWwindow* window;
+const uint32_t WIDTH = 400;
+const uint32_t HEIGHT = 300;
+
 
 
 void printStats(const VkPhysicalDevice & device) {
@@ -65,9 +69,47 @@ void printStats(const VkPhysicalDevice & device) {
         std::cout << "Image transfer granularity:  " << width << ", " << height << ", " << depth << std::endl;
     }
 
-    delete[] familyProperties;
+    VkSurfaceCapabilitiesKHR surfaceCapabilities;
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &surfaceCapabilities);
+    std::cout << std::endl << "Surface capabilities:" << std::endl;
+    std::cout << "\tminImageCount:           " << surfaceCapabilities.minImageCount << std::endl;
+    std::cout << "\tmaxImageCount:           " << surfaceCapabilities.maxImageCount << std::endl;
+    std::cout << "\tcurrentExtent:           " << surfaceCapabilities.currentExtent.width << "/" << surfaceCapabilities.currentExtent.height << std::endl;
+    std::cout << "\tminImageExtent:          " << surfaceCapabilities.minImageExtent.width << "/" << surfaceCapabilities.minImageExtent.height << std::endl;
+    std::cout << "\tmaxImageExtent:          " << surfaceCapabilities.maxImageExtent.width << "/" << surfaceCapabilities.maxImageExtent.height << std::endl;
+    std::cout << "\tmaxImageArrayLayers:     " << surfaceCapabilities.maxImageArrayLayers << std::endl;
+    std::cout << "\tsupportedTransforms:     " << surfaceCapabilities.supportedTransforms << std::endl;
+    std::cout << "\tcurrentTransform:        " << surfaceCapabilities.currentTransform << std::endl;
+    std::cout << "\tsupportedCompositeAlpha: " << surfaceCapabilities.supportedCompositeAlpha << std::endl;
+    std::cout << "\tsupportedUsageFlags:     " << surfaceCapabilities.supportedUsageFlags << std::endl;
+
+    // COLOR FORMATS
+    uint32_t amountOfFormats = 0;
+    vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &amountOfFormats, nullptr);
+    VkSurfaceFormatKHR* surfaceFormats = new VkSurfaceFormatKHR[amountOfFormats];
+    vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &amountOfFormats, surfaceFormats);
+    std::cout << std::endl;
+    std::cout << "Amount of formats: " << amountOfFormats << std::endl;
+
+    for (uint32_t i = 0; i < amountOfFormats; ++i)
+    {
+        std::cout << "Format: " << surfaceFormats[i].format << std::endl;
+    }
+
+    uint32_t amountOfPresentationModes = 0;
+    vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &amountOfPresentationModes, nullptr);
+    VkPresentModeKHR* presentModes = new VkPresentModeKHR[amountOfPresentationModes];
+    vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &amountOfPresentationModes, presentModes);
+    std::cout << std::endl;
+    std::cout << "Amount of presentation modes: " << amountOfPresentationModes << std::endl;
+    for (uint32_t i = 0; i < amountOfPresentationModes; ++i) {
+        std::cout << "Supported presentation mode: " << presentModes[i] << std::endl;
+    }
 
     std::cout << std::endl;
+    delete[] familyProperties;
+    delete[] surfaceFormats;
+    delete[] presentModes;
 };
 
 void startGLFW()
@@ -76,7 +118,7 @@ void startGLFW()
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    window = glfwCreateWindow(400, 300, "Vulkan Hello World", nullptr, nullptr);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Hello World", nullptr, nullptr);
 }
 
 void startVulkan()
@@ -183,6 +225,10 @@ void startVulkan()
     // Features to enable on the device
     VkPhysicalDeviceFeatures usedFeatures = {};
 
+    const std::vector<const char*> deviceExtensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+
     VkDeviceCreateInfo deviceCreateInfo;
     deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
     deviceCreateInfo.pNext = nullptr;
@@ -191,17 +237,49 @@ void startVulkan()
     deviceCreateInfo.pQueueCreateInfos = &deviceQueueCreateInfo; // Enhance if more queue families needed
     deviceCreateInfo.enabledLayerCount = 0;
     deviceCreateInfo.ppEnabledLayerNames = nullptr;
-    deviceCreateInfo.enabledExtensionCount = 0;
-    deviceCreateInfo.ppEnabledExtensionNames = nullptr;
+    deviceCreateInfo.enabledExtensionCount = deviceExtensions.size();
+    deviceCreateInfo.ppEnabledExtensionNames = deviceExtensions.data();
     deviceCreateInfo.pEnabledFeatures = &usedFeatures;
 
     // CREATE DEVICE
     result = vkCreateDevice(physicalDevices[0], &deviceCreateInfo, nullptr, &device); //TODO: pick best device - instead of first device
     ASSERT_VULKAN(result);
 
+    //CHECK IF DEVICES SUPPORTS SURFACE
+    VkBool32 surfaceSupport = false;
+    result = vkGetPhysicalDeviceSurfaceSupportKHR(physicalDevices[0], 0, surface, &surfaceSupport);
+    ASSERT_VULKAN(result);
+
+    if (!surfaceSupport) {
+        std::cerr << "Surface not supported!" << std::endl;
+        __debugbreak();
+    }
+
     VkQueue queue;
     // Choose family index correct (look way up)
     vkGetDeviceQueue(device, 0, 0, &queue);
+
+    VkSwapchainCreateInfoKHR swapchainCreateInfo;
+    swapchainCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    swapchainCreateInfo.pNext = nullptr;
+    swapchainCreateInfo.flags = 0;
+    swapchainCreateInfo.surface = surface;
+    swapchainCreateInfo.minImageCount = 3; // TODO check dynamically for graphicscard
+    swapchainCreateInfo.imageFormat = VK_FORMAT_B8G8R8A8_UNORM; // TODO check dynamically for graphicscard
+    swapchainCreateInfo.imageColorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR; // TODO check dynamically for graphicscard
+    swapchainCreateInfo.imageExtent = VkExtent2D{WIDTH, HEIGHT};
+    swapchainCreateInfo.imageArrayLayers = 1;
+    swapchainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+    swapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE; //TODO check
+    swapchainCreateInfo.queueFamilyIndexCount = 0;
+    swapchainCreateInfo.pQueueFamilyIndices = nullptr;
+    swapchainCreateInfo.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+    swapchainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR; // window is not transparent
+    swapchainCreateInfo.presentMode = VK_PRESENT_MODE_FIFO_KHR;
+    swapchainCreateInfo.clipped = VK_TRUE; // clip pixels outside of the image
+    swapchainCreateInfo.oldSwapchain = VK_NULL_HANDLE;
+
+    result = vkCreateSwapchainKHR(device, &swapchainCreateInfo, nullptr, &swapchain);
 
     delete[] layers;
     delete[] extensions;
@@ -220,6 +298,7 @@ void shutdownVulkan()
     vkDeviceWaitIdle(device);
 
     // Destroy after all tasks done
+    vkDestroySwapchainKHR(device, swapchain, nullptr);
     vkDestroyDevice(device, nullptr);
     vkDestroySurfaceKHR(instance, surface, nullptr);
     vkDestroyInstance(instance, nullptr);
